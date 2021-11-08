2c2663e89468f6984bbe237284ee623533208dba
Thomas Gelf <thomas@gelf.net>
2021-11-08 22:26:02 +0100

    SystemdStdoutWriter: add facility, default local0

--- src/Writer/SystemdStdoutWriter.php
+++ src/Writer/SystemdStdoutWriter.php
@@ -4,15 +4,23 @@ namespace gipfl\Log\Writer;
 
 use gipfl\Log\LogLevel;
 use gipfl\Log\LogWriter;
+use InvalidArgumentException;
 use React\EventLoop\LoopInterface;
 use React\Stream\WritableResourceStream;
 use React\Stream\WritableStreamInterface;
+use function is_int;
 use function sprintf;
 
 class SystemdStdoutWriter implements LogWriter
 {
+    // local0
+    const DEFAULT_FACILITY = 10;
+
     /** @var WritableStreamInterface */
     protected $stdOut;
+
+    /** @var int */
+    protected $facility = self::DEFAULT_FACILITY;
 
     /**
      * SystemdStdoutWriter constructor.
@@ -26,6 +34,20 @@ class SystemdStdoutWriter implements LogWriter
         } else {
             $this->stdOut = $stdOut;
         }
+    }
+
+    /**
+     * @param int $facility
+     */
+    public function setFacility($facility)
+    {
+        if (! is_int($facility)) {
+            throw new InvalidArgumentException('Facility needs to be an integer');
+        }
+        if ($facility < 0 || $facility > 23) {
+            throw new InvalidArgumentException("Facility needs to be between 0 and 23, got $facility");
+        }
+        $this->facility = $facility;
     }
 
     public function write($level, $message)
