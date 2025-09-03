519b6044a32751b207ca4a677adfc74b9a486990
Thomas Gelf <thomas@gelf.net>
2025-09-03 11:00:53 +0200

    PsrVersion: it's not our composer, we need to...
    ...check directly

--- src/PsrVersion.php
+++ src/PsrVersion.php
@@ -3,11 +3,37 @@
 namespace gipfl\Log;
 
 use Composer\InstalledVersions;
+use Psr\Log\LoggerInterface;
+use RuntimeException;
 
 class PsrVersion
 {
+    protected const SEARCH_FILE = 'Psr/Log/LoggerInterface.php';
     public static function isV3(): bool
     {
-        return PHP_VERSION_ID >= 80000 && version_compare(InstalledVersions::getVersion('psr/log'), '3.0.0.0', '>=');
+        if (PHP_VERSION_ID < 80000) {
+            return false;
+        }
+
+        $installed = InstalledVersions::getVersion('psr/log');
+        if ($installed !== null) {
+            return version_compare(InstalledVersions::getVersion('psr/log'), '3.0.0.0', '>=');
+        }
+
+        if (interface_exists(LoggerInterface::class)) {
+            $length = strlen(self::SEARCH_FILE);
+
+            foreach (get_required_files() as $filename) {
+                if (substr($filename, - $length) === self::SEARCH_FILE) {
+                    if (strpos(file_get_contents($filename), 'Stringable') !== false) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        throw new RuntimeException('psr/log has not been found. Please check your dependencies');
     }
 }
